102915894246431d0d17252d313f8f80b36cb9ca
Drishya <drishya.tm@qxf2.com>
2020-07-23 14:42:51 +0530

    renamed the tutorial main page to login page and made changes to all reflective area

---
+++ page_objects/codecademy_login_page.py
@@ -0,0 +1,19 @@
+"""
+This class models the codecademy login page.
+URL: login
+
+"""
+from utils.Wrapit import Wrapit
+from .Base_Page import Base_Page
+from .codecademy_login_page_objects import Codecademy_Login_Page_Objects
+
+
+
+class Codecademy_Login_page(Base_Page, Codecademy_Login_Page_Objects):
+    "Page Object for the tutorial's main page"
+
+    def start(self):
+        "Use this method to go to specific URL -- if needed"
+        url = 'login'
+        print("launching the page")
+        self.open(url)
