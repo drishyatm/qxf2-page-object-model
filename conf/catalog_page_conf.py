a380e46a2638a05a5226fd8029cba3bc33db1684
Drishya <drishya.tm@qxf2.com>
2020-07-23 16:09:30 +0530

    Modified/added the page to the take inputs from conf file

---
+++ conf/catalog_page_conf.py
@@ -0,0 +1,2 @@
+course_name = "SQL"
+redirect_title_course = "SQL Courses & Tutorials"
--- page_objects/codecademy_catalog_page.py
+++ page_objects/codecademy_catalog_page.py
@@ -3,10 +3,10 @@ This class models the redirect page of the Selenium tutorial
 URL: selenium-tutorial-redirect
 The page consists of a header, footer and some text
 """
+from utils.Wrapit import Wrapit
 from .Base_Page import Base_Page
 import conf.locators_conf as locators
-from utils.Wrapit import Wrapit
-
+import conf.catalog_page_conf as courses
 
 class Codecademy_Catalog_Page(Base_Page):
     "Page Object for the Catalog page"
@@ -14,7 +14,8 @@ class Codecademy_Catalog_Page(Base_Page):
     # locators
     heading_catalog = locators.heading_catalog
     sql_course_path = locators.sql_course_path
-    redirect_title_course = "SQL Courses & Tutorials"
+    course_name = courses.course_name
+    redirect_title_course = courses.redirect_title_course
 
     def start(self):
         "Use this method to go to specific URL -- if needed"
@@ -36,7 +37,7 @@ class Codecademy_Catalog_Page(Base_Page):
     @Wrapit._screenshot
     def click_sql_course(self):
         " Click the SQL course in catalog page"
-        result_flag = self.click_element(self.sql_course_path)
+        result_flag = self.click_element(self.sql_course_path%self.course_name)
         self.conditional_write(result_flag,
                                positive='Clicked on the SQL course ',
                                negative='Could not click on the SQL course',
@@ -47,7 +48,7 @@ class Codecademy_Catalog_Page(Base_Page):
     @Wrapit._exceptionHandler
     @Wrapit._screenshot
     def check_redirect(self):
-        "Check if we have been redirected to the redirect page"
+        "Check if we have been redirected to the course page"
         result_flag = False
         print(self.driver.title)
         if self.redirect_title_course in self.driver.title:
